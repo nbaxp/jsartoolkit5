50b529c2d46c9a20838b1d9a6202c0a9149a8fba
wanggang <76527413@qq.com>
2021-07-22 21:19:31 +0800

    update

---
+++ MarkerTex/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿
+namespace MarkerTex
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(679, 23);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(698, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 24);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "select";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(263, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Select extraction level for tracking features :";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(292, 51);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 23);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(273, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Select extraction level for initializing features :";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(292, 85);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 23);
+            this.numericUpDown2.TabIndex = 5;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(226, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Enter the minimum image resolution :";
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(292, 118);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(120, 23);
+            this.numericUpDown3.TabIndex = 7;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 154);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(229, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Enter the maximum image resolution :";
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(292, 152);
+            this.numericUpDown4.Maximum = new decimal(new int[] {
+            720,
+            0,
+            0,
+            0});
+            this.numericUpDown4.Minimum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(120, 23);
+            this.numericUpDown4.TabIndex = 9;
+            this.numericUpDown4.Value = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(698, 53);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 24);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "gen";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(698, 96);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 24);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "rename";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ MarkerTex/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace MarkerTex
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.textBox1.Text = this.folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(this.textBox1.Text))
+            {
+                return;
+            }
+            var files = Directory.GetFiles(this.textBox1.Text, "*.jpg");
+            var path = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            var processName = Path.Combine(path, "genTexData.exe");
+            int i = 0;
+            foreach (var item in files)
+            {
+                try
+                {
+                    var minDef = (int)this.numericUpDown3.Value;
+                    var maxDef = (int)this.numericUpDown4.Value;
+                    int? minRes = null;
+                    int? maxRes = null;
+                    using var process = new Process();
+
+                    process.StartInfo.WorkingDirectory = textBox1.Text;
+                    process.StartInfo.FileName = "cmd.exe";
+                    process.StartInfo.UseShellExecute = false;
+                    process.StartInfo.CreateNoWindow = true;
+                    process.StartInfo.RedirectStandardInput = true;
+                    process.StartInfo.RedirectStandardOutput = true;
+                    process.StartInfo.RedirectStandardError = true;
+                    process.EnableRaisingEvents = true;
+
+                    process.OutputDataReceived += (s, e) =>
+                    {
+                        if (e.Data != null)
+                        {
+                            Debug.WriteLine(e.Data);
+                            //if (e.Data.StartsWith("Select extraction level for tracking features"))
+                            //{
+                            //    (s as Process).StandardInput.WriteLine(this.numericUpDown1.Value.ToString());
+                            //}
+                            if (e.Data.StartsWith("Enter the minimum image resolution"))
+                            {
+                                if (!minRes.HasValue || !maxRes.HasValue)
+                                {
+                                    var res = Regex.Match(e.Data, @"\[([^,]+),([^,]+)\]");
+                                    var min = (int)Math.Ceiling(double.Parse(res.Groups[1].Value));
+                                    var max = (int)Math.Floor(double.Parse(res.Groups[2].Value));
+                                    if (min < minDef && max > minDef)
+                                    {
+                                        minRes = minDef;
+                                    }
+                                    else
+                                    {
+                                        minRes = min;
+                                    }
+                                    if (minRes < maxDef && max > maxDef)
+                                    {
+                                        maxRes = maxDef;
+                                    }
+                                    else
+                                    {
+                                        maxRes = max;
+                                    }
+                                    Debug.WriteLine($"RES:{minDef}-{maxDef},{min}-{max},{minRes}-{maxRes}");
+                                }
+                            }
+                        }
+                    };
+                    process.ErrorDataReceived += (s, e) =>
+                    {
+                        if (e.Data != null)
+                        {
+                            Debug.WriteLine(e.Data);
+                        }
+                    };
+                    process.Exited += (s, e) =>
+                    {
+                        Debug.WriteLine(e.ToString());
+                    };
+
+                    process.Start();
+                    process.StandardInput.AutoFlush = true;
+                    process.BeginErrorReadLine();
+                    process.BeginOutputReadLine();
+                    process.StandardInput.WriteLine($"{processName} \"{item}\" && exit");
+                    process.StandardInput.WriteLine(this.numericUpDown1.Value.ToString());
+                    process.StandardInput.WriteLine(this.numericUpDown2.Value.ToString());
+                    process.StandardInput.WriteLine();
+                    process.StandardInput.Close();
+                    process.WaitForExit();
+                    process.CancelErrorRead();
+                    process.CancelOutputRead();
+                    process.Close();
+
+                    process.Start();
+                    process.StandardInput.AutoFlush = true;
+                    process.BeginErrorReadLine();
+                    process.BeginOutputReadLine();
+                    process.StandardInput.WriteLine($"{processName} \"{item}\" && exit");
+                    process.StandardInput.WriteLine(this.numericUpDown1.Value.ToString());
+                    process.StandardInput.WriteLine(this.numericUpDown2.Value.ToString());
+                    process.StandardInput.WriteLine(minRes.Value);
+                    process.StandardInput.WriteLine(maxRes.Value);
+                    process.StandardInput.Close();
+                    process.WaitForExit();
+                    process.Close();
+                    i++;
+                    this.Text = i.ToString();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            Debug.WriteLine("done");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var files = Directory.GetFiles(this.textBox1.Text, "*.jpg");
+            var i = 1;
+            foreach (var item in files)
+            {
+                var dist = Path.Combine(Path.GetDirectoryName(item), $"{i}.jpg");
+                File.Move(item, dist);
+                i++;
+            }
+        }
+    }
+}
